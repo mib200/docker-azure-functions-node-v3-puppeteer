7956848a51c861f0a9a46a7bedbeba2a60c1b354
Manish Kumar <manishkumar@MacBook-Pro-New.local>
2020-04-18 20:28:19 +0530

    updated function

--- html-to-pdf/index.js
+++ html-to-pdf/index.js
@@ -1,4 +1,6 @@
+const os = require('os');
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 const { sendErrorResponse } = require('../common/utils');
 
 module.exports = async (ctx, req) => {
@@ -10,34 +12,40 @@ module.exports = async (ctx, req) => {
     return sendErrorResponse(context, 400, 'Please send `html` or `url` string in request body.');
   }
 
+  let tempDir = os.tmpdir();
+  let tempFile = `${tempDir}/temp_${Date.now()}.pdf`;
+
   try {
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    // await page.setViewport({ width: 1240, height: 1754 });
-    // await page.goto(`file:${Path.join(__dirname, htmlPath)}`);
-    if (url) await page.goto(url, { waitUntil: 'networkidle0' });
-    else if (html) await page.setContent(html);
-
-    const pdf = await page.pdf({
-      // preferCSSPageSize: true,
-      // printBackground: true,
-      displayHeaderFooter: true,
-      footerTemplate: '<span></span>',
-      printBackground: true,
-      format: 'A4',
-      // width: '1240px',
-      // height: '1754px',
-    // width: '1060px',
-    // height: '1500px',
-    });
-    context.res = {
-      status: 200,
-      headers: { 'Content-Type': 'application/pdf', 'Content-Length': pdf.length, 'Content-Disposition': 'attachment; filename=download.pdf' },
-      isRaw: true,
-      body: pdf,
-    };
-
-    return context.done();
+    await (async () => {
+      const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+      const page = await browser.newPage();
+      // await page.setViewport({ width: 1240, height: 1754 });
+      // await page.goto(`file:${Path.join(__dirname, htmlPath)}`);
+      if (url) await page.goto(url, { waitUntil: 'networkidle0' });
+      else if (html) await page.setContent(html);
+      
+      await page.pdf({
+        path: tempFile,
+        // preferCSSPageSize: true,
+        // printBackground: true,
+        displayHeaderFooter: true,
+        footerTemplate: '<span></span>',
+        printBackground: true,
+        format: 'A4',
+        // width: '1240px',
+        // height: '1754px',
+        // width: '1060px',
+        // height: '1500px',
+      });
+      await browser.close();
+      context.res = {
+        status: 200,
+        headers: { 'Content-Type': 'application/pdf', 'Content-Disposition': 'attachment; filename=download.pdf' },
+        isRaw: true,
+        body: fs.readFileSync(tempFile)
+      };
+      context.done();
+    })();
   } catch (e) {
     const error = { ...e };
     context.log(e);
